b36b20fd7e3834b28ec0a0a99c514fee0153aa3c
Ca5e <roossienkees@gmail.com>
2024-02-18 22:55:03 +0100

    Initial setup

---
+++ __tests__/template.test.js
@@ -0,0 +1,28 @@
+import { expect } from "chai"
+import RegExT from "../lib/index.js"
+
+const regexps = [
+  /(?:(?<=[^`\\])|^)\[(?=[^@\n\]]+\]\([^@)]*@[:a-z0-9_-]*\))(?<showtext>[^@\n\]]+)\]\((?:(?:(?<type>[a-z0-9_-]*):)?)(?:(?<term>[^@\n:#)]*?)?(?:#(?<trait>[^@\n:#)]*))?)?@(?<scopetag>[a-z0-9_-]*)(?::(?<vsntag>[a-z0-9_-]*))?\)/g,
+  /(?:(?<=[^`\\])|^)\[(?=[^@\n\]]+?@[:a-z0-9_-]*\](?:\([#:a-z0-9_-]+\))?)(?<showtext>[^@\n\]]+?)@(?<scopetag>[a-z0-9_-]*)(?::(?<vsntag>[a-z0-9_-]*?))?\](?:\((?:(?:(?<type>[a-z0-9_-]+):)?)(?<term>[^@\n:#)]*?)(?:#(?<trait>[^@\n:#)]+?))?\))?/
+]
+
+const templates = [
+  "[{{showtext}}]({{#type}}{{type}}:{{/type}}{{term}}{{#trait}}#{{trait}}{{/trait}}@{{scopetag}}{{#vsntag}}:{{vsntag}}{{/vsntag}})",
+  "[showtext](type:term#trait@scopetag:vsntag)",
+  "[{{showtext}}@{{scopetag}}{{#vsntag}}:{{vsntag}}{{/vsntag}}]{{#term}}({{#type}}{{type}}:{{/type}}{{term}}{{#trait}}#{{trait}}{{/trait}}){{/term}}",
+  "[showtext@scopetag:vsntag](type:term#trait)"
+]
+
+const modes = ["Mustache", "plain"]
+
+describe("Templates", () => {
+  regexps.forEach((regexp, i) => {
+    modes.forEach((mode, j) => {
+      const query = new RegExT(regexp, mode).template
+      const template = templates[i * modes.length + j]
+      it(`Template ${i * modes.length + j + 1}: '${regexp.toString().slice(0, 20)}...${regexp.toString().slice(-20)}' > '${template.slice(0, 20)}...${template.toString().slice(-20)}`, () => {
+        expect(query).to.equal(template)
+      })
+    })
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,96 @@
-import { parse, traverse, type CapturingGroup } from "regexp-tree"
+import pkg from "regexp-tree"
+const { traverse, transform } = pkg
+import { Char } from "regexp-tree/ast"
+
+/**
+ * A class that represents a regular expression as a template string
+ * @param regexp - The regular expression to convert
+ * @param type - The type of template string to create
+ */
+export default class RegExTemplate {
+  public regexp: RegExp
+  public type: "Mustache" | "plain"
+  public template: string
+
+  constructor(regexp: RegExp, type: "Mustache" | "plain" = "Mustache") {
+    this.regexp = regexp
+    this.type = type
+    this.template = convert(regexp, type)
+  }
+
+  toString(): string {
+    return this.template
+  }
+}
+
+/**
+ * Convert a regular expression to a template string
+ * @param regexp - The regular expression to convert
+ * @param type - The type of template string to convert to
+ * @returns The template string
+ */
+export function convert(regexp: RegExp, type: "Mustache" | "plain" = "Mustache") {
+  const ast = transform(regexp, {
+    // Remove certain types of nodes from the AST
+    "*": function (path) {
+      const type = path.node.type
+      if (type === "CharacterClass" || type === "Disjunction" || type === "Assertion") {
+        path.remove()
+      }
+    },
+    // Replace capturing groups with template strings
+    Group(path) {
+      const { node } = path
+      if (node.capturing) {
+        if (type === "Mustache") {
+          path.replace(createCharNode(`{{${node.name || node.number}}}`))
+        } else {
+          path.replace(createCharNode(`${node.name || node.number}`))
+        }
+      }
+    },
+    // Add template strings to alternatives that contain capturing groups
+    Alternative(path) {
+      const { node, parentPath } = path
+      if (!parentPath?.parentPath?.parentPath) {
+        return
+      }
+      let capturing: string | number | null = null
+
+      for (let i = 0; i < node.expressions.length; i++) {
+        const expression = node.expressions[i]
+        if (expression.type === "Group") {
+          if (expression.capturing) {
+            capturing = expression.name || expression.number
+            break
+          }
+        }
+      }
+      if (capturing) {
+        if (type === "Mustache") {
+          node.expressions.unshift(createCharNode(`{{#${capturing}}}`))
+          node.expressions.push(createCharNode(`{{/${capturing}}}`))
+        }
+      }
+    }
+  })
+
+  // Convert the AST to a template string
+  let template = ""
+  traverse(ast.getAST(), {
+    Char({ node }) {
+      template += node.value
+    }
+  })
+
+  return template
+}
+
+// Helper function to create a Char node
+const createCharNode = (value: string) =>
+  ({
+    type: "Char",
+    value,
+    kind: "simple",
+    codePoint: NaN
+  }) as Char
